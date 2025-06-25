3d6a12d105b3cf09ebc67cd20e93b256d2090bdb
sillonjeu <pius0316@naver.com>
2025-06-25 10:50:31 +0900

    feat: 로그인/회원가입 로직 구축

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,69 @@
 "use client";
 
 import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useUserStore } from "@/stores/userStore";
 
 export default function Home() {
   const [isLogin, setIsLogin] = useState(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const router = useRouter();
+  const setUser = useUserStore((s) => s.setUser);
+
+  const API = process.env.NEXT_PUBLIC_API_BASE_URL; // env 변수
+
+  const handleLoginSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const res = await fetch(`${API}/api/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (!res.ok) {
+      console.error("로그인 실패", await res.text());
+      return;
+    }
+
+    const data = await res.json();
+    const { user_id, nickname, token } = data;
+
+    setUser({
+      user_id,
+      nickname,
+      token,
+      email: "",
+      password: "",
+    });
+
+    router.replace("/main");
+  };
+
+  const handleSignUpSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      alert("비밀번호가 일치하지 않습니다.");
+      return;
+    }
+
+    const res = await fetch(`${API}/api/signup`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (!res.ok) {
+      console.error("회원가입 실패", await res.text());
+      return;
+    }
+    router.replace("/login");
+  };
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gray-50">
@@ -19,38 +79,85 @@ export default function Home() {
       </header>
 
       <main className="w-full max-w-sm bg-white shadow-md rounded-lg p-8">
-        <form className="flex flex-col gap-4">
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-700"
-            >
-              이메일
-            </label>
-            <input
-              type="email"
-              id="email"
-              placeholder="example@email.com"
-              className="w-full mt-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
+        {isLogin ? (
+          <form onSubmit={handleLoginSubmit} className="flex flex-col gap-4">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                이메일
+              </label>
+              <input
+                type="email"
+                id="email"
+                placeholder="example@email.com"
+                className="w-full mt-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
 
-          <div>
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-gray-700"
-            >
-              비밀번호
-            </label>
-            <input
-              type="password"
-              id="password"
-              placeholder="비밀번호를 입력하세요"
-              className="w-full mt-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                비밀번호
+              </label>
+              <input
+                type="password"
+                id="password"
+                placeholder="비밀번호를 입력하세요"
+                className="w-full mt-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
 
-          {!isLogin && (
+            <button
+              type="submit"
+              className="w-full py-2 mt-4 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 transition"
+            >
+              로그인
+            </button>
+          </form>
+        ) : (
+          <form onSubmit={handleSignUpSubmit} className="flex flex-col gap-4">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                이메일
+              </label>
+              <input
+                type="email"
+                id="email"
+                placeholder="example@email.com"
+                className="w-full mt-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                비밀번호
+              </label>
+              <input
+                type="password"
+                id="password"
+                placeholder="비밀번호를 입력하세요"
+                className="w-full mt-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+
             <div>
               <label
                 htmlFor="confirm-password"
@@ -63,28 +170,30 @@ export default function Home() {
                 id="confirm-password"
                 placeholder="비밀번호를 다시 입력하세요"
                 className="w-full mt-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                required
               />
             </div>
-          )}
 
-          <button
-            type="submit"
-            className="w-full py-2 mt-4 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 transition"
-          >
-            {isLogin ? "로그인" : "회원가입"}
-          </button>
-
-          <div className="text-sm text-center text-gray-600 mt-4">
-            {isLogin ? "계정이 없으신가요?" : "이미 계정이 있으신가요?"}{" "}
             <button
-              type="button"
-              className="text-blue-500 hover:underline"
-              onClick={() => setIsLogin(!isLogin)}
+              type="submit"
+              className="w-full py-2 mt-4 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 transition"
             >
-              {isLogin ? "회원가입 하기" : "로그인 하기"}
+              회원가입
             </button>
-          </div>
-        </form>
+          </form>
+        )}
+
+        <div className="text-sm text-center text-gray-600 mt-4">
+          {isLogin ? "계정이 없으신가요?" : "이미 계정이 있으신가요?"}{" "}
+          <button
+            type="button"
+            className="text-blue-500 hover:underline"
+            onClick={() => setIsLogin(!isLogin)}
+          >
+            {isLogin ? "회원가입 하기" : "로그인 하기"}
+          </button>
+        </div>
       </main>
     </div>
   );
