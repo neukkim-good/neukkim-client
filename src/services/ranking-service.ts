3d5af03fc6b922bc0e78086d07305db5b082cad3
Dayoung331 <110375535+Dayoung331@users.noreply.github.com>
2025-06-26 15:06:21 +0900

    배포된 서버 접속

--- src/services/ranking-service.ts
+++ src/services/ranking-service.ts
@@ -2,7 +2,7 @@
 // ex. fetchTodayRankingData, fetchWeeklyRankingData, etc.
 
 export async function fetchRankingData() {
-  const data = await fetch("http://3.34.95.59/ranking")
+  const data = await fetch("http://3.34.95.59:3001/ranking")
     .then((response) => response.json())
     .catch((error) => {
       console.error("Error fetching ranking data:", error);
