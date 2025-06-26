1bfeb7367626db0520b159bbb0f8954c98db69b4
kmkkkp <kmkkkp@ajou.ac.kr>
2025-06-26 10:50:42 +0900

    UPDATE button design

--- src/app/(game)/select-game/page.tsx
+++ src/app/(game)/select-game/page.tsx
@@ -5,21 +5,20 @@ export default function RankingPage() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gray-50">
       <header className="text-center">
-        <h1 className="text-4xl font-bold text-gray-800">게임 선택</h1>
-        <p className="text-gray-500 mt-2">이 부분 코드 잘 작성해주세요~</p>
+        <h1 className="text-4xl font-bold text-gray-800">🍏게임 선택🍎</h1>
       </header>
       <main>
-        <Link
-          href="/apple-game"
-          className="block mb-4 text-blue-600 hover:underline"
-        >
-          개인 플레이
+        <Link href="/apple-game">
+          <button className="w-full p-2 mt-4 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 transition">
+            개인 플레이
+          </button>
         </Link>
+
         <button
-          className="block mb-4 text-blue-600 hover:underline"
+          className="w-full p-2 mt-4 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 transition"
           onClick={() => alert("준비 중입니다!")}
         >
-          1:1 플레이
+          멀티 플레이
         </button>
       </main>
     </div>
