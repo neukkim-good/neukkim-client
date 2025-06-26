a8439ab7b5076b8d6550948930b7b9d3f00fee72
sillonjeu <pius0316@naver.com>
2025-06-26 09:18:21 +0900

    feat: Provider 로그인/비로그인 시 로직 수정

--- src/components/layouts/Navbar/Navbar.tsx
+++ src/components/layouts/Navbar/Navbar.tsx
@@ -1,18 +1,22 @@
 "use client";
 
+import Link from "next/link";
 import { Bell, User } from "lucide-react";
 
 export default function Navbar() {
   return (
     <nav className="w-full h-16 px-6 flex items-center justify-between bg-white shadow">
-      <div className="text-xl font-bold text-black">로고 자리</div>
+      <Link href="/" className="text-xl font-bold text-black">
+        로고 자리
+      </Link>
+
       <div className="flex items-center gap-4">
-        <button title="공지사항">
+        <Link href="/notice" title="공지사항">
           <Bell className="w-6 h-6 text-gray-700 hover:text-black transition" />
-        </button>
-        <button title="마이페이지">
+        </Link>
+        <Link href="/mypage" title="마이페이지">
           <User className="w-6 h-6 text-gray-700 hover:text-black transition" />
-        </button>
+        </Link>
       </div>
     </nav>
   );
