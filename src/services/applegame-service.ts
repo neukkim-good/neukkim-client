29bbb675d71a9ec5a89c302d6a2d05d2b6814ed0
Dayoung331 <110375535+Dayoung331@users.noreply.github.com>
2025-06-27 16:55:39 +0900

    사과판 받아오고 내기사과페이지에서 점수 보내기

--- src/services/applegame-service.ts
+++ src/services/applegame-service.ts
@@ -21,3 +21,32 @@ export async function sendGameResult(score: number) {
     throw error;
   }
 }
+
+export async function sendBettingResult(score: number, room_id: string) {
+  try {
+    const token = sessionStorage.getItem("token");
+
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/room/${room_id}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+          "Bearer-token": `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          score,
+          room_id,
+        }),
+      }
+    );
+
+    if (!res.ok) throw new Error("내기 결과 저장에 실패했습니다.");
+
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log("내기 저장 실패, 오류 원인:", error);
+    throw error;
+  }
+}
