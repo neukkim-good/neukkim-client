3af29cd32922ed6ab8f9359ab49f5eeafb34f7ae
kmkkkp <kmkkkp@ajou.ac.kr>
2025-06-27 15:20:43 +0900

    UPDATE socket address

--- src/app/(game)/room/[room_id]/page.tsx
+++ src/app/(game)/room/[room_id]/page.tsx
@@ -20,6 +20,8 @@ export default function RoomDetailPage() {
   //   if (!room) return <p>로딩 중…</p>;
 
   const [room, setRoom] = useState<RoomDetail | null>(null);
+  const [token, setToken] = useState<string | null>(null);
+  const [socketId, setSocketId] = useState<string | null>(null);
 
   useEffect(() => {
     if (!room_id) return;
@@ -37,15 +39,17 @@ export default function RoomDetailPage() {
       );
   }, [room_id]);
 
-  const token = sessionStorage.getItem("token");
+  useEffect(() => {
+    setToken(sessionStorage.getItem("token"));
+  });
   // if (room) console.log("사과게임 판: ", room?.board);
 
-  const socket = io(`ws://localhost:3002`, {
+  const socket = io(`ws://${process.env.NEXT_PUBLIC_WS_URL}:3002`, {
     path: "/socket.io",
     transports: ["websocket"],
   });
   socket.on("connect", () => {
-    // console.log("Socket connected:", socket.id);
+    setSocketId(socket.id ? socket.id : null);
   });
   const sendMessage = (message: string) => {
     socket.emit("message", { room_id, message, token, socket_id: socket.id });
