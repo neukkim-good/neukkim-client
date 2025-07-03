91d65c5d82c0a15ca8c170ea4fa27f160acd4f1c
sillonjeu <pius0316@naver.com>
2025-07-03 21:38:08 +0900

    feat: 방 만들기 시간 수정

--- src/app/(game)/createRoom/page.tsx
+++ src/app/(game)/createRoom/page.tsx
@@ -8,7 +8,10 @@ export default function CreateRoomPage() {
   const [token, setToken] = useState<string | null>("");
   const [selectHour, setSelectHour] = useState("8");
   const router = useRouter();
-  const timeList = [8, 9, 10, 11, 13, 14, 15, 16];
+  const timeList = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23,
+  ];
 
   // 시간 가공 함수
   function getToday() {
